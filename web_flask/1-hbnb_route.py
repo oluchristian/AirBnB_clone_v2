e3ac77394c12b49f803828d002542024dd22c634
Ughagwu Oluchukwu <oluchristian98@gmail.com>
2024-05-17 02:06:11 +0100

    created a route /hbnb

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Starts a flask web application
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def home():
+    """_summary_"""
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """_summary_
+    """
+    return "HBNB"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
