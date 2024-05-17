1123a83e8739c49bce1c1f6d43c7a648713bb8d6
Ughagwu Oluchukwu <oluchristian98@gmail.com>
2024-05-17 02:11:56 +0100

    fixed pycodestyle errors

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -5,16 +5,19 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def home():
     """_summary_"""
     return "Hello HBNB!"
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """_summary_
     """
     return "HBNB"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
